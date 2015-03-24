66291daad6a04550fab3d63f4299ceb37379b2c4
mcox02 <matthew.ford.cox@gmail.com>
2015-03-24 16:53:39 -0400

    First pass at sample viz

--- script/script.js
+++ script/script.js
@@ -1,90 +1,74 @@
 /*Start by setting up the canvas */
-var margin = {t:50,r:50,b:100,l:50};
-var width = $('.canvas').width() - margin.r - margin.l,
+var margin = {t:100,r:50,b:200,l:50},
+    width = $('.canvas').width() - margin.l - margin.r,
     height = $('.canvas').height() - margin.t - margin.b;
 
-var canvas = d3.select('.canvas')
+
+//Set up SVG drawing elements -- already done
+var svg = d3.select('.canvas')
     .append('svg')
-    .attr('width',width+margin.r+margin.l)
-    .attr('height',height + margin.t + margin.b)
+    .attr('width', width + margin.l + margin.r)
+    .attr('height', height + margin.t + margin.b)
     .append('g')
-    .attr('class','canvas')
     .attr('transform','translate('+margin.l+','+margin.t+')');
 
 
-//Setting up scales
-var scaleX = d3.scale.linear().domain([1962,2011]).range([0,width]),
-    scaleY = d3.scale.linear().domain([0,40000000]).range([height,0]);
+//Global Variables
+    var yVariable = "GF",
+        y0 = 1964,
+        y1 = 2004;
+//Scales
+
+//d3 map
+    var 
+//arc generator
+    var arcGenerator = d3.svg.arc();
+
+//layout Functions
+    var sunburst = d3.layout.partition()
+        .children(function(d){
+            return d.values;
+        })
+        .value(function(d){
+            return d.data.get(y1);
+        })
+        .size([width,height]);
 
 
+
+
+
+//START!
 queue()
-    .defer(d3.csv,"data/FAO_world_apple_production.csv",parse)
-    .defer(d3.csv,"data/FAO_area_classification.csv",parseMetadata)
+    .defer(d3.csv, "DataSheets/IndividualYears/leagues_NHL_2004_teams.csv",parse)
+    .defer(d3.csv, "DataSheets/IndividualYears/leagues_NHL_1964_teams.csv",parse)
     .await(dataLoaded);
 
+
+function dataLoaded(err, 1694, 2004){
+
+
+}
+
+function draw(root){
+
+
+}
+
 function parse(d){
-    return {
-        areaCode: +d.AreaCode,
-        areaName: d.AreaName,
-        year: +d.Year,
-        value: +d.Value
-    };
-}
+    if(d.Team && d.GF){
+        return {
+            team: d.Team,
+            goalsFor: +d.GF,
+            wins: +d.W,
+            goalsAgainst: +d.GA,
 
-function parseMetadata(d){
-    return d;
-}
-
-function dataLoaded(err,data,metadata){
-    //Check to see how many 
-    //console.log(data);
-
-    //create a nest function
-    var nest = d3.nest()
-        .key(function(d){ return d.areaName; });
-
-    //this is data nested by country
-    var nestedData = nest.entries(data);
-
-    //console.log(nestedData);
-
-    draw(nestedData);
-}
-
-function draw(data){
-
-    //create svg line generator
-    var line = d3.svg.line()
-        .x(function(d){
-            return scaleX(d.year);
-        })
-        .y(function(d){
-            return scaleY(d.value);
-        })
-        .interpolate('cardinal');
-
-    var area = d3.svg.area()
-        .x(function(d){
-            return scaleX(d.year);
-        })
-        .y(function(d){
-            return scaleY(d.value);
-        })
-        .y0(height);
-
-
-    canvas.selectAll('.country')
-        .data(data)
-        .enter()
-        .append('path')
-        .attr('d',function(d){
-            return area(d.values)
-        })
-        .style('fill', 'green')
-        .style('stroke','black')
-        .style('stroke-width', '3px')
-        .style('opacity','.25');
-
-
+            }
+    }else{
+        return;
+    }
 
 }
+
+
+
