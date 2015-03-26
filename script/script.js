cbb26289e0ed65bdc288a65aa845c8d51b93b5c3
mcox02 <matthew.ford.cox@gmail.com>
2015-03-26 00:33:49 -0400

    Need help with domain scaling.

--- script/script.js
+++ script/script.js
@@ -14,15 +14,29 @@ var svg = d3.select('.canvas')
 
 
 //Global Variables
-    var yVariable = "GF",
+var yVariable = "GF",
         y0 = 1964,
         y1 = 2004;
+
+var radius = Math.min(25, 300);
+
+
 //Scales
+var scaleX = d3.scale.linear()
+    .range([0, 2* Math.PI]);
+
+var scaleY = d3.scale.sqrt()
+    .range([0,radius]);
 
 //d3 map
-    var 
+ var map = d3.map();
+
 //arc generator
-    var arcGenerator = d3.svg.arc();
+    var arc = d3.svg.arc()
+    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, scaleX(d.x))); })
+    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, scaleX(d.x + d.dx))); })
+    .innerRadius(function(d) { return Math.max(0, scaleY(d.y)); })
+    .outerRadius(function(d) { return Math.max(0, scaleY(d.y + d.dy)); });
 
 //layout Functions
     var sunburst = d3.layout.partition()
@@ -30,7 +44,7 @@ var svg = d3.select('.canvas')
             return d.values;
         })
         .value(function(d){
-            return d.data.get(y1);
+            return d.goalsFor;
         })
         .size([width,height]);
 
@@ -45,14 +59,43 @@ queue()
     .await(dataLoaded);
 
 
-function dataLoaded(err, 1694, 2004){
+function dataLoaded(err, rows0){
 
+ scaleX.domain( d3.extent(rows0,function(d){ return d.goalsFor; }));
+ scaleY.domain( d3.extent(rows0, function(d){ return d.wins; }));
+
+    var rows0 = d3.nest()
+        .key(function(d){
+            return d.conference;
+        })
+        .entries(rows0);
+
+    var root = {
+        key:"conference",
+        values: rows0
+    };
+console.log(rows0);
+console.log(root);
+
+    draw(root);
 
 }
 
 function draw(root){
-
-
+    svg.append('g')
+        .attr ('transform','translate('+width/2+','+height/2+')')
+        .selectAll('.slice')
+        .data(sunburst(root))
+        .enter()
+        .append('path')
+        .attr('class','slice')
+        .attr('d', function(d){
+            return arc(d);
+        })
+        .style('fill', 'gray')
+        .style('stroke','green')
+        .style('stroke-width','2px')
+        .style('fill-opacity','.25');
 }
 
 function parse(d){
@@ -62,6 +105,7 @@ function parse(d){
             goalsFor: +d.GF,
             wins: +d.W,
             goalsAgainst: +d.GA,
+            conference: d.Conference,
 
             }
     }else{
