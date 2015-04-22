060de217c7db9f059813c5188f830332a4c47918
mcox02 <matthew.ford.cox@gmail.com>
2015-04-22 14:35:48 -0400

    Final update for presentation.

--- script/script-2.js
+++ script/script-2.js
@@ -1,26 +1,86 @@
+/*
+  Current Needs
+    
+    Simplifying code
+        especially depth bar
+  
+    Color inside slices?
+
+    Update data with last 2.5 years
+
+    Tooltip
+      Hide list values/titles when not in use
+      Add thousands comma to make it easier to read
+
+    !! - Combine depth bar and tooltip?
+*/
+
+/*
+  Current Bugs
+    
+    Depth bar text is black
+      Tried doing css styles, and in line changing
+
+    Year slices not sorted chronologically
+
+    Not all text showing up
+      Conference/Division/Team is only writing text 
+      for one year in each era
+
+    When text would be Horizontal, it's rotating to Veritcal
+*/
+
+/*
+  Ideas for Future
+
+    Preselected historical teams
+        Islanders Dynasty
+        Broad Street Bullies
+        Big Bad Bruins
+        Wayne Gretzky teams
+
+    Recap of each season selected
+        President's Trophy
+        Stanley Cup Winner   
+
+    Bio for each team selected
+        Founding Year
+        Folding Year
+        Stanley Cups
+        A different name (Thrashers -> Jets)
+
+    Comparison to average for that year    
+
+
+*/
+
+
+
 //Set up Canvas
 var margin = {t:0,r:0,b:0,l:0},
-	width = $('.canvas').width() - margin.l - margin.r,
-	height = $('.canvas').height() - margin.t - margin.b,
-	radius = Math.min(width,height)/2,
-	x = d3.scale.linear()
-		.range([0,2*Math.PI]),
-	y = d3.scale.sqrt()
-		.range([0,radius]),
-	duration = 1000,
-	padding = 5;
+  width = $('.canvas').width() - margin.l - margin.r,
+  height = $('.canvas').height() - margin.t - margin.b,
+  radius = Math.min(width,height)/2,
+  x = d3.scale.linear()
+    .range([0,2*Math.PI]),
+  y = d3.scale.sqrt()
+    .range([0,radius]),
+  duration = 1000,
+  padding = 5;
 
 var svg = d3.select('.canvas')
-	.append('svg')
-	.attr('width', width + margin.l + margin.r)
+  .append('svg')
+  .attr('width', width + margin.l + margin.r)
   .attr('height', height + margin.t + margin.b)
   .append('g')
   .attr('transform','translate('+margin.l+','+margin.t+')');
 
 var map = d3.map(),
-	metaDataMap = d3.map();
+  metaDataMap = d3.map();
 
 var customTooltip = d3.select('.tooltip');
+
+var currentDepth = 0;
 
 
 
@@ -50,7 +110,7 @@ queue()
 
 function dataLoaded (err, data, metaData){
 
-	var data = d3.nest()
+  var data = d3.nest()
         .key(function(d){
             return d.era;
         })
@@ -74,121 +134,281 @@ function dataLoaded (err, data, metaData){
     draw(root);
 }
 
-function draw (root){	
+function draw (root){ 
 
 x.domain([0,width]);
 y.domain([0,height]);
 
 
-	console.log(root);
-/*
-    to add text:
+  console.log(root);  
 
-    select each slice
-    append text element to each slice
-      text element is based off of d.key
-      for leaves it is d.team
-    rotate each text element to match slice
-
-    use tween function to update text elements
-      change visibility to hide unshown nodes
-*/
-  svg.selectAll('.slice')
-    .data(sunburst.nodes(root), function(d){
-                  if (!d.children){ return d.team }
-                  else { return d.key }
-                    })
-    .enter()
-    .append('text')
-    
-
-
-
-/*	var nodes = sunburst.nodes({children: root});
+/*  var nodes = sunburst.nodes({children: root});
 console.log(nodes);*/
-	var slice = svg.selectAll('.slice')
-		.data(sunburst.nodes(root))
-		.enter()
-		.append('path')
-		.attr('class', 'slice')
-		.attr ('transform','translate('+width/2+','+height/2+')')
-		.attr('d',arc)
-		.attr('fill-rule','evenodd')
+  var slice = svg.selectAll('.slice')
+    .data(sunburst.nodes(root))
+    .enter();
+  var sliceEnter =  slice.append('path')
+    .attr('class', 'slice')
+    .attr ('transform','translate('+width/2+','+height/2+')')
+    .attr('d',arc)
+    .attr('fill-rule','evenodd')
         .style('fill',function(d){
             var team = (metaDataMap.get(d.team))
 
-            if (!team) {return 'gray'}
+            if (!team) {return 'white'}
             else {return ( 'rgb(' + (team.teamPrimaryColor) + ')')}
         })
         .style('stroke',function(d){
             var team = (metaDataMap.get(d.team))
 
-            if (!team) {return 'green'}
+            if (!team) {return 'gray'}
             else {return ( 'rgb(' + (team.teamSecondaryColor) + ')')}
         })
-		.on('click',click);
+        .style('opacity',function(d){
+            return  (d.depth * 0.2)
+            
+        })
+        .style('stroke-width','4px')
+    .on('click',click);
 
-	var text = svg.selectAll("text")
-		.data(sunburst.nodes(root), function(d){return d.key})
-    	.enter()
-    	.append("text")
-      	.style("fill-opacity", 1)
-      	.style("fill", function(d) {
-        return brightness(d3.rgb(color(d))) < 125 ? "#eee" : "#000";
-      })
+  var text = svg.selectAll(".text")
+    .data(sunburst.nodes(root), function(d){return d.key})
+      .enter()
+      .append('g')
+      .attr('class','text')
+      .attr('transform','translate('+width/2+','+height/2+')')
+      .append("text")
+        .style("fill-opacity", 1)
+        .style("fill", "rgb(250,250,250)")
       .attr("text-anchor", function(d) {
         return x(d.x + d.dx / 2) > Math.PI ? "end" : "start";
       })
       .attr("dy", ".2em")
       .attr("transform", function(d) {
-      	//console.log(d.team);
+        //console.log(d.team);
         var multiline = (d.team || "").split(" ").length > 1,
             angle = x(d.x + d.dx / 2) * 180 / Math.PI - 90,
             rotate = angle + (multiline ? -0.5 : 0);
-        return "rotate(" + rotate + ")translate(" + (y(d.y) + padding) + ")rotate(" + (angle > 90 ? -180 : 0) + ")";
+        return "rotate("+rotate+")translate(" + (y(d.y) + padding) + ")rotate(" + (angle > 90 ? -180 : 0) + ")";
       })
       .on("click", click);
-  text.append("tspan")
-      .attr("x", 0)
-      .text(function(d) { return d.depth ? d.team.split(" ")[0] : ""; });
-  text.append("tspan")
-      .attr("x", 0)
-      .attr("dy", "1em")
-      .text(function(d) { return d.depth ? d.team.split(" ")[1] || "" : ""; });
 
-  function click(d) {
-      //console.log("Enter" + d.properties.NAME);
+    text.append("tspan")
+      .attr("x", 0)
+      .text(function(d) {
+            if ((+d.depth)>((+currentDepth) + 1)){return}
+              else if ((+d.depth) === 5){ return d.team}
+                else {return d.key}
+      });
+  // text
+  //     .attr("x", 0)
+  //     .attr("dy", "1em")
+  //     .text(function(d) { return d.depth ? d.key.split(" ")[1] || "" : ""; });
+
+    var depthBar = d3.select('.depthBar')
+      .append('svg')
+      .attr('width', '100px')
+      .attr('height', '700px')
+      .append('g')
+      .attr('transform','translate(0,25)');
+
+    depthBar.append('rect')
+      .attr('id','depth-1')
+      .style('opacity','.25')
+      .attr('width','25px')
+      .attr('height','110px')
+      .attr('x','0px')
+      .attr('y','25px')
+      .attr('fill','white');
+      depthBar.append('text')
+      .attr('text-anchor','left')
+      .attr('x','30px')
+      .attr('y','80px')
+      .text('Era');
+
+    depthBar.append('rect')
+      .attr('id','depth-2')
+      .style('opacity','.25')
+      .attr('width','25px')
+      .attr('height','110px')
+      .attr('x','0px')
+      .attr('y','150px')
+      .attr('fill','white');      
+    depthBar.append('text')
+      .attr('text-anchor','left')
+      .attr('x','30px')
+      .attr('y','205px')
+      .text('Year');
+
+    depthBar.append('rect')
+      .attr('id','depth-3')
+      .style('opacity','.25')
+      .attr('width','25px')
+      .attr('height','110px')
+      .attr('x','0px')
+      .attr('y','275px')
+      .attr('fill','white');
+    depthBar.append('text')
+      .attr('text-anchor','left')
+      .attr('x','30px')
+      .attr('y','330px')
+      .text('Conf.');
+
+    depthBar.append('rect')
+      .attr('id','depth-4')
+      .style('opacity','.25')
+      .attr('width','25px')
+      .attr('height','110px')
+      .attr('x','0px')
+      .attr('y','400px')
+      .attr('fill','white');
+    depthBar.append('text')
+      .attr('text-anchor','left')
+      .attr('x','30px')
+      .attr('y','455px')
+      .text('Division');
+
+    depthBar.append('rect')
+      .attr('id','depth-5')
+      .style('opacity','.25')
+      .attr('width','25px')
+      .attr('height','110px')
+      .attr('x','0px')
+      .attr('y','525px')
+      .attr('fill','white');
+    depthBar.append('text')
+      .attr('text-anchor','left')
+      .attr('x','30px')
+      .attr('y','580px')
+      .text('Team');
+
+
+function click(d) {
       //show tooltip if hidden
+      function checkDepth(d) {
+            if ((+d.depth)>((+currentDepth) + 1)){return}
+              else if ((+d.depth) === 5){ return d.team}
+                else {return d.key}};
+
     d3.select('.tooltip')
-        .style('visibility','visible')
-    //find out where on the screen the tooltip needs to go
-
-
+        .style('visibility','visible');
+  
     console.log(d);
+    currentDepth = (d.depth);
+
+    var depthBar = d3.select('.depthBar');
+
+    text.selectAll("tspan")
+      .attr("x", 0)
+      .text(checkDepth);
 
     //inject data into content of tooltip
-    var id = + function(d) {
-      console.log(d);
+    if(+currentDepth==1){
+      depthBar.select('#depth-1')
+      .style('opacity','1')
+    }
+    else{depthBar.select('#depth-1')
+      .style('opacity','.25');
+    }
 
-      if (!d.key){ return d.team }
-        else{return d.key}
-    }   
-    
 
-    
+    if(+currentDepth==2){
+      depthBar.select('#depth-1')
+      .style('opacity','1')
+      depthBar.select('#depth-2')
+      .style('opacity','1')
+    }
+    else{depthBar.select('#depth-2')
+      .style('opacity','.25');
+    }
+
+    if(+currentDepth==3){
+      depthBar.select('#depth-1')
+      .style('opacity','1')
+      depthBar.select('#depth-2')
+      .style('opacity','1')
+      depthBar.select('#depth-3')
+      .style('opacity','1')
+    }
+    else{depthBar.select('#depth-3')
+      .style('opacity','.25');
+    }
+
+        if(+currentDepth==4){
+      depthBar.select('#depth-1')
+      .style('opacity','1')
+      depthBar.select('#depth-2')
+      .style('opacity','1')
+      depthBar.select('#depth-3')
+      .style('opacity','1')
+      depthBar.select('#depth-4')
+      .style('opacity','1')
+    }
+    else{depthBar.select('#depth-4')
+      .style('opacity','.25');
+    }
+
+        if(+currentDepth==5){
+      depthBar.select('#depth-1')
+      .style('opacity','1')
+      depthBar.select('#depth-2')
+      .style('opacity','1')
+      depthBar.select('#depth-3')
+      .style('opacity','1')
+      depthBar.select('#depth-4')
+      .style('opacity','1')
+      depthBar.select('#depth-5')
+      .style('opacity','1')
+    }
+    else{depthBar.select('#depth-5')
+      .style('opacity','.25');
+    }
+
+    if(+currentDepth<5){
     customTooltip
-        .select('h2')
+        .select('.teamName')
+        .html(d.key);
+        }   
+    else{customTooltip
+        .select('.teamName')
         .html(d.team);
-    customTooltip
-        .select('span')
+      }
+
+
+    var table = customTooltip.select('.data-table')
+
+      table.select('.era')
+        .html('Era')
+      table.select('.eraValue')
+        .html(d.era)
+      table.select('.year')
+        .html('Year')
+      table.select('.yearValue')
+        .html(d.year)
+      table.select('.conference')
+        .html('Conference')
+      table.select('.conferenceValue')
+        .html(d.conference)
+      table.select('.division')
+        .html('Division')
+      table.select('.divisionValue')
+        .html(d.division)
+      table.select('.gamesPlayed')
+        .html('Games Played')
+      table.select('.gamesPlayedValue')
+        .html(d.gamesPlayed)
+      table.select('.goalsScored')
+        .html('Goals Scored')
+      table.select('.goalsScoredValue')
         .html(d.value)
 
-    slice.transition()
+
+    sliceEnter.transition()
       .duration(duration)
       .attrTween("d", arcTween(d));
-;
+  
 
-	text.style("visibility", function(e) {
+  text.style("visibility", function(e) {
           return isParentOf(d, e) ? null : d3.select(this).style("visibility");
         })
       .transition()
@@ -210,29 +430,30 @@ console.log(nodes);*/
         .each("end", function(e) {
           d3.select(this).style("visibility", isParentOf(d, e) ? null : "hidden");
         });
-  }
-
-
+}
 
 }
+
+
 
 function parse(d){
-        return {
-            era: d.Era,
-            year: d.Year,
-            team: d.TeamName,
-            conference: d.Conference,
-            division: d.Division,
-            gamesPlayed: +d.GP,
-            wins: +d.Wins,
-            goalsFor: +d.GF,
-            goalsAgainst: +d.GA,
-            penaltyMinutes: +d.PIM,
-            powerPlayPercent: d["PP%"],
-            penaltyKillPercent: d["PK%"],
+        if(d.TeamName){
+            return {
+              era: d.Era,
+              year: d.Year,
+              team: d.TeamName,
+              conference: d.Conference,
+              division: d.Division,
+              gamesPlayed: +d.GP,
+              wins: +d.Wins,
+              goalsFor: +d.GF,
+              goalsAgainst: +d.GA,
+              penaltyMinutes: +d.PIM,
+              powerPlayPercent: d["PP%"],
+              penaltyKillPercent: d["PK%"],
        };
 
-}
+}}
 
 function parseMetaData(d){
 //     console.log(d.teamName,d.primaryColor);
@@ -251,7 +472,7 @@ function parseMetaData(d){
 
 function color(d){
 
-  console.log(d.children);
+  //console.log(d.children);
   if (d.children) {
     var colors = d.children.map(color),
         a = d3.hsl(colors[0]),
@@ -294,29 +515,3 @@ function brightness(rgb) {
   return rgb.r * 0.299 + rgb.g * 0.587 + rgb.b * 0.114;
 }
 
-function onMouseEnter(d){
-    //console.log("Enter" + d.properties.NAME);
-  //show tooltip if hidden
-    d3.select('.custom-tooltip')
-        .style('visibility','visible')
-    //find out where on the screen the tooltip needs to go
-
-    var xy = d3.mouse(d3.select('.canvas').node());
-        customTooltip
-            .style('left',xy[0]+'px')
-            .style('top', xy[1]+'px');
-
-    //inject data into content of tooltip
-    var id = (+d.properties.STATE) + d.properties.COUNTY,   
-        rate = rateById.get(id);
-    
-
-    
-    customTooltip
-        .select('h2')
-        .html(d.properties.NAME);
-    customTooltip
-        .select('span')
-        .html(rate)
-
-}
